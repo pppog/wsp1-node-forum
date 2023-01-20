76b0fab1fe3e8902a0ee9e1b1493206067af75d2
pppog <loke.oberg@elev.ga.ntig.se>
2023-01-20 09:41:04 +0100

    hello

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async function (req, res) {
-    res.send('Hello you!')
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+
+const promisePool = pool.promise();
+
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM ja15forum");
+    res.json({ rows });
+});
+
+router.get('/new', async function (req, res, next) {
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+    });
+});
+
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+    const [rows] = await promisePool.query("INSERT INTO lo28forum (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
+    res.redirect('/');
+});
+
+res.render('index.njk', {
+    rows: rows,
+    title: 'Forum',
 });
 
 module.exports = router;
